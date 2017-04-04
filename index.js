ba72a7cebe437cf6f4b247748ffc57376bd47387
Chris Bianca <me@chrisbianca.com>
2017-04-04 17:44:39 +0100

    Add getAssets method with flow definitions

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
+// @flow
 import { NativeModules } from 'react-native';
 
-export default NativeModules.RNCameraRoll;
+const RNCameraRollModule = NativeModules.RNCameraRoll;
+
+type AssetType = "image" | "video" | "all";
+
+type GetAssetsParams = {
+  assetType?: AssetType;
+  first: number;
+  start?: string | number;
+}
+
+type Location = {
+  altitude?: number;
+  heading?: number;
+  latitude: number;
+  longitude: number;
+  speed?: number;
+}
+
+type Asset = {
+  filename: string;
+  height: number;
+  location: Location;
+  timestamp: number;
+  type: AssetType;
+  uri: string;
+  width: number;
+}
+
+type PageInfo = {
+  end_cursor: string | number;
+  has_next_page: boolean;
+  start_cursor: string | number;
+}
+
+type GetAssetsResponse = {
+  assets: Array<Asset>;
+  page_info: PageInfo;
+}
+
+/**
+ * @class RNCameraRoll
+ */
+export default class RNCameraRoll {
+  /**
+   * @param params
+   * @returns {Promise}
+   */
+  static getAssets(params: GetAssetsParams) : Promise<GetAssetsResponse> {
+    return RNCameraRollModule.getAssets(params);
+  }
+}
